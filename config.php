981c0b06535969064abb49afa3ec987d48589846
amrij <a.m.rijkeboer@am-rijkeboer.nl>
2019-04-22 13:00:49 +0200

    Zonnepanelen.php is samengevoegd met zonnepanelen-electra.php
    Zonnepanelen is samengevoged met zonnepanelen-electra.php.
    aangepast config.php: $zonnestysteem-electra.gif is nu $zonnestysteem.gif ($zonnestysteem.gif  is gelijk aan $zonnestysteem-electra.gif)
    toegevoegd config.php: $P1 = 0  // P1 waarden aangeven nee = 0, ja =1
    aangepast zonnepanelen.css: dit is een aangepaste versie van zonnepanelen-electra.css
    aangepast zonepanelen.php: dit is een aangepaste versie van zonnepanelen-electra.php

--- config.php
+++ config.php
@@ -19,49 +19,51 @@
 // along with zonnepanelen.  If not, see <http://www.gnu.org/licenses/>.
 //
 // versie: 1.26
-// auteurs: André Rijkeboer en Jos van der Zande
+// auteur: André Rijkeboer
 // datum:  13-04-2019
 // omschrijving: configuratie bestand
 
 // gegevens voor het openen van de database
-$host = '192.168.1.181'; // IP adres waar de database staat (standaard localhost of 127.0.0.1)
+$host = '192.168.1.81'; // IP adres waar de database staat (standaard localhost of 127.0.0.1)
 $port = '3306'; // poort database (standaard 3306)
 $user = 'gebruiker'; // gebruiker
-$passwd = 'password'; // paswoord gebruiker
+$passwd = 'paswoord gebruiker'; // paswoord gebruiker
 $db = 'solaredge'; // database naam
 
-// gegevens van de plaats waar de zonnepanelen staan
+// gegeven van de plaats waar de zonnepanelen staan
 $lat = 51.9515885; //Latitude North
 $long = 6.0045953; //Longitude East
 
 // Achtergrond image (in de img directory)
 $zonnesysteem = "zonnesysteem.gif";
 
-//#### Toegevoegd voor zonnepanelen-electra.php tbv informatie ophalen van electra
+//#### Toegevoegd voor P1 ElectriciteitsMeter informatie van Domoticz
+//** P1 waarden aangeven 
+$P1 = 0; // 0 = nee, 1 = ja
+
 //** Algemene velden
-$ElecLeverancier = "Delta";                         // naam electra leverancier
-$ElecDagGraph = '60';                               // aantal dagen in grafiek
-$ElecMaandGraph = '13';                             // aantal maanden in grafiek
-$zonnesysteem_electra = "zonnesysteem-electra.gif"; // achtergrond. Let op: dit is een ander formaat dan het orgineel om het ook op Mobiel te kunnen laten zien.
+$ElecLeverancier = "Engie";                            // naam electra leverancier
+$ElecDagGraph = '60';                                   // aantal dagen in grafiek
+$ElecMaandGraph = '13';                                 // aantal maanden in grafiek
 
 //** velden die worden gebruikt om de PVGis schatting in de website te laten zien. Wordt alleen getoond als ze invult zijn.
-$PVGtxt = "PVGis";                                  // Tekst waar de schatting vandaan komt bv: "PVGis"
-$PVGis = [0,0,0,0,0,0,0,0,0,0,0,0];                 // schatting opbrengst iedere maand voor de installatie
+$PVGtxt = "Eigen";                                           // Tekst waar de schatting vandaan komt bv: "PVGis"
+$PVGis = [91,215,441,635,863,787,873,711,518,260,104,47];  // schatting opbrengst iedere maand voor de installatie
 
 //** velden voor ophalen P1 info van Domoticz server
 $domohost = '192.168.0.??:8080';                    // ip:poort van domoticz
 $domoidx = "123";                                   // device IDX voor de Electriciteits P1 meter
 $DataURL = 'live-server-data-electra-domoticz.php'; // URL voor ophalen p1&Converter data tbv zonnepanelen-electra.php
 
-//** velden voor Electra info van DSMR server (verwijder // van de volgende regels om deze optie te activeren)
+//** velden voor Electra info van DSMR server(verwijder // om te activeren)
 // $dsmr_url='http://host-ip:8888';                     // URL voor DSMR inclusief
 // $dsmr_apikey='IDkdjqljwdlkqjwdoiiqjdpockskskdxpF';   // APIKEY voor DSMR
 // $DataURL = 'live-server-data-electra-dsmr.php';      // URL voor ophalen p1&Converter data tbv zonnepanelen-electra.php
 
-//** velden voor Electra info van eigen database server (verwijder // van de volgende regel om deze optie te activeren)
-// $DataURL = 'live-server-data-electra-p1_meter_table.php';      // URL voor ophalen p1&Converter data tbv zonnepanelen-electra.php
+//** velden voor Electra info van eigen database server(verwijder // om te activeren)
+// $DataURL = 'live-server-data-electra-p1_meter_table.php';     // URL voor ophalen electra&Converter data
 
-//#### einde extra velden tbv zonnepanelen-electra.php
+//#### einde aanpassing
 
 // aangeven vermogen op het paneel
 $vermogen = 1; // 0 = nee, 1 = ja
@@ -69,32 +71,32 @@ $vermogen = 1; // 0 = nee, 1 = ja
 // gegevens van het zonnepanelensysteem
 $inverter = 3; // 1 voor enkel fase en 3 voor 3 fase inverter
 $naam = "SolarEdge SE7k"; //naam van de inverter
-// optimizer id en positie paneel, de richting van de panelen Vertikaal = 0, Horizontaal = 1
+//optimizer id en positie paneel, de richting van de panelen Vertikaal = 0, Horizontaal = 1
 // het serienummer en het vermogen van het paneel
-// $op_id[id optimizer][inverter.string.paneelnummer][richting][id paneel][vermogen paneel]
-$op_id[1] = ['2020B2E3','1.1.1',1,'D5',300];
-$op_id[2] = ['2020B18C', '1.1.2',1,'D7',300];
-$op_id[3] = ['2020B1B2', '1.1.3',1,'A3',300];
-$op_id[4] = ['2020B353', '1.1.4',1,'46',300];
-$op_id[5] = ['2020B202', '1.1.5',1,'F4',300];
-$op_id[6] = ['2020B1FE', '1.1.6',1,'EF',300];
-$op_id[7] = ['2020B2A5', '1.1.7',1,'97',300];
-$op_id[8] = ['2020B1B0', '1.1.8',1,'A1',300];
-$op_id[9] = ['2020B25B', '1.1.9',1,'4D',300];
-$op_id[10] = ['2020B14E', '1.1.10',1,'3F',300];
-$op_id[11] = ['2020B2CB', '1.1.11',1,'BD',300];
-$op_id[12] = ['2020B1ED', '1.1.12',1,'DE',300];
-$op_id[13] = ['2020B369', '1.1.13',1,'5C',300];
-$op_id[14] = ['2020B34F', '1.1.14',1,'42',300];
-$op_id[15] = ['2020B22F', '1.1.15',1,'21',300];
-$op_id[16] = ['2020B161', '1.1.16',1,'52',300];
-$op_id[17] = ['2020B3A4', '1.1.17',1,'97',300];
-$op_id[18] = ['2020B25F', '1.1.18',1,'51',300];
-$op_id[19] = ['2020B387', '1.1.19',1,'7A',300];
-$op_id[20] = ['2020B362', '1.1.20',1,'55',300];
-$op_id[21] = ['20212017', '1.1.21',1,'78',300];
-$op_id[22] = ['20212166', '1.1.22',1,'C8',300];
-$op_id[23] = ['20211F32', '1.1.23',1,'92',300];
-$op_id[24] = ['20211FD6', '1.1.24',1,'36',300];
+//$op_id[id optimizer][inverter.string.paneelnummer][richting][id paneel][vermogen paneel]
+$op_id[1] = ['2020B2E3','1.1.1',1,'SL220M1509EE0155kX',265];
+$op_id[2] = ['2020B18C', '1.1.2',1,'SL220M1509EE0149Qv',265];
+$op_id[3] = ['2020B1B2', '1.1.3',1,'SL220M1509EE0059tX',265];
+$op_id[4] = ['2020B353', '1.1.4',1,'SL220M1509EE0162Od',265];
+$op_id[5] = ['2020B202', '1.1.5',1,'SL220M1509EE0060Qd',265];
+$op_id[6] = ['2020B1FE', '1.1.6',1,'SL220M1509EE0158tX',265];
+$op_id[7] = ['2020B2A5', '1.1.7',1,'SL220M1509EE0156mX',265];
+$op_id[8] = ['2020B1B0', '1.1.8',1,'SL220M1509EE01538X',265];
+$op_id[9] = ['2020B25B', '1.1.9',1,'DM5511502050664',265];
+$op_id[10] = ['2020B14E', '1.1.10',1,'DM5511502050260',265];
+$op_id[11] = ['2020B2CB', '1.1.11',1,'DM5511503231463',265];
+$op_id[12] = ['2020B1ED', '1.1.12',1,'DM5511503230750',265];
+$op_id[13] = ['2020B369', '1.1.13',1,'DM5511503230845',265];
+$op_id[14] = ['2020B34F', '1.1.14',1,'DM5511503230749',265];
+$op_id[15] = ['2020B22F', '1.1.15',1,'DM5511503231553',265];
+$op_id[16] = ['2020B161', '1.1.16',1,'DM5511502050094',265];
+$op_id[17] = ['2020B3A4', '1.1.17',1,'DM5511503231269',265];
+$op_id[18] = ['2020B25F', '1.1.18',1,'DM5511503231231',265];
+$op_id[19] = ['2020B387', '1.1.19',1,'DM5511503231278',265];
+$op_id[20] = ['2020B362', '1.1.20',1,'DM5511503231165',265];
+$op_id[21] = ['20212017', '1.1.21',1,'DM5511503231557',265];
+$op_id[22] = ['20212166', '1.1.22',1,'DM5511503231281',265];
+$op_id[23] = ['20211F32', '1.1.23',1,'DM5511503231176',265];
+$op_id[24] = ['20211FD6', '1.1.24',1,'DM5511503231339',265];
 $aantal = count($op_id); // aantal zonnepanelen dat in database is opgenomen
 ?>
