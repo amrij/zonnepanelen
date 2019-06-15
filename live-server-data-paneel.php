4c97e95ef06f35de98e230cc8271f0c24fc5b671
amrij <a.m.rijkeboer@gmail.com>
2019-06-15 20:20:19 +0200

    Updated version and moved to master

--- live-server-data-paneel.php
+++ live-server-data-paneel.php
@@ -18,9 +18,9 @@
 // You should have received a copy of the GNU General Public License
 // along with zonnepanelen.  If not, see <http://www.gnu.org/licenses/>.
 //
-// versie: 1.69.0
+// versie: 1.69.2
 // auteur: André Rijkeboer
-// datum:  30-05-2019
+// datum:  15-06-2019
 // omschrijving: ophalen van de stroom en energie gegevens van de panelen en de inverter (1 dag)
 
 include('config.php');
